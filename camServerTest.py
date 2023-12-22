d93694b9a67452f3b98c9c88e8d08da5f50ccaec
seon <effortwoong5@kakao.com>
2023-12-22 17:23:49 +0900

    fc

---
+++ camServerTest.py
@@ -0,0 +1,20 @@
+import socket
+import cv2
+
+UDP_IP = "172.17.0.1"
+UDP_PORT = 6000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    d = frame.flatten()
+    s = d.tostring()
+
+    for i in range(20):
+        sock.sendto(s[i*46080:(i+1)*46080], (UDP_IP, UDP_PORT))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
---
+++ flask_app.py
@@ -0,0 +1,25 @@
+import socket
+import numpy
+import cv2
+
+UDP_IP = "172.17.0.1"
+UDP_PORT = 6000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+s = b''
+
+while True:
+    data, addr = sock.recvfrom(46080)
+    s += data
+
+    if len(s) == (46080 * 20):
+        frame = numpy.fromstring(s, dtype=numpy.uint8)
+        frame = frame.reshape(480, 640, 3)
+        cv2.imshow("frame", frame)
+        s = b''
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
